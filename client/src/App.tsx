23774e63c72670bbc0fb22d1eed44201a02b6f04
Lucas Gomes <lucasgomes.dky@gmail.com>
2021-05-20 10:56:03 -0300

    Config styled-components, eslint, prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { GlobalStyle } from './style-guide/GlobalStyle'
+import * as S from './styles'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+const App = () => (
+  <>
+    <GlobalStyle />
+    <S.Container>
+      <S.Header>
+        <S.Logo />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        <S.Link>Learn React</S.Link>
+      </S.Header>
+    </S.Container>
+  </>
+)
 
-export default App;
+export default App
